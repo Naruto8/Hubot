49562a63d6653be1593ebde3c7fc9e0429426c88
Rohith Mukku <rohithmukku@gmail.com>
2016-11-28 21:21:31 +0530

    Removed tokens

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ echo "Password: "
 stty -echo
 read  passd
 stty echo
-env HUBOT_SLACK_TOKEN=xoxb-103012040741-fT6ErwSNG9TfK8zzWFTbGsvk HUBOT_EMAIL_ID=pratikab@iitk.ac.in HUBOT_EMAIL_USER=$user HUBOT_EMAIL_PWD=$passd EMAIL_LABEL=inbox EMAIL_FETCH_INTERVAL=1 ./bin/hubot -a slack
+env HUBOT_SLACK_TOKEN=<slack token id> HUBOT_EMAIL_ID=<your email id or Hubot email id> HUBOT_EMAIL_USER=$user HUBOT_EMAIL_PWD=$passd EMAIL_LABEL=inbox EMAIL_FETCH_INTERVAL=1 ./bin/hubot -a slack
